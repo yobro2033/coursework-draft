f0affde34e746daf4d0d28ee0e2f3325fe92da79
yobro2033 <80685605+yobro2033@users.noreply.github.com>
2021-09-25 19:30:55 +0700

    Update morrisons_module.py

--- StoreModule/morrisons_module.py
+++ StoreModule/morrisons_module.py
@@ -4,46 +4,46 @@ import json
 
 productInput = input("Please enter your product you want to find: ")
 productURLInput = productInput.replace(" ","%20")
-my_url = 'https://groceries.morrisons.com/search?entry=' + productURLInput
+my_url = 'https://groceries.morrisons.com/webshop/api/v1/search?searchTerm=' + productURLInput
 response = requests.get(my_url)
 responseCode = str(response)
 if responseCode == "<Response [200]>":
-    print("We have found your product: " + productInput)
     productListFound = soup(response.text, 'html.parser')
-    containers = productListFound.findAll('div',{"class":"fop-item fop-item-offer"})
-    for container in containers:
-        productLinkContainer = container.findAll('div',{"class":"fop-contentWrapper"})
-        productLinkItem = productLinkContainer[0].a["href"]
-        productLink = 'https://groceries.morrisons.com' + productLinkItem
-        productImageContainer = container.find('div',{"class":"fop-img-wrapper"})
-        productImageRef = productImageContainer.img["src"]
-        productImage = 'https://groceries.morrisons.com' + productImageRef
-        productName = productImageContainer.div.img["alt"]
-        price_container = container.find('span',{"class":"fop-price price-offer"})
-        productPrice = price_container.text
-        #Some cases there is an extra pattern of HTML
-        #Traceback (most recent call last):
-        #   File "morrisons_module.py", line 23, in <module>
-        #   productPrice = price_container.text
-        #   AttributeError: 'NoneType' object has no attribute 'text'
-        unitprice_container = container.find('span',{"class":"fop-unit-price"})
-        productUnitPrice = unitprice_container.text
-        #Some cases there is an extra pattern of HTML
-        print("Product link: ", productLink)
-        print("Product name: ", productName)
-        print("Image: ",productImage)
-        print("Price: ", productPrice)
-        print("Price per unit: ", productUnitPrice)
-        morrisons_table_data = []
-        morrisons_table_data.append({
-            'Product link: ': [productLink],
-            'Product name: ': [productName],
-            'Image': [productImage],
-            'Price: ': [productPrice],
-            'Price per unit: ': [productUnitPrice]
-        })
-        with open('m_data.txt', 'w') as outfile:
-            json.dump(morrisons_table_data, outfile)
+    productListJson = json.loads(str(productListFound))
+    if 'mainFopCollection' in productListJson:
+        print("We have found your product: " + productInput)
+        containers = productListJson['mainFopCollection']['sections']
+        for container in containers:
+            productContainer = container['fops']
+            for container in productContainer:
+                productSku = container['sku']
+                productLink = 'https://groceries.morrisons.com/products/-' + productSku
+                if 'product' in container:
+                    productName = container['product']['name']
+                    productImage = 'https://groceries.morrisons.com/productImages/116/116564011_0_640x640.jpg'
+                    productPrice = container['product']['price']['current']
+                    productUnitPrice = container['product']['price']['unit']['price']
+                    unit = container['product']['price']['unit']['per']
+                    print("Product link: ", productLink)
+                    print("Product name: ", productName)
+                    print("Image: ",productImage)
+                    print("Price: ", productPrice)
+                    print("Price per unit: ", productUnitPrice, unit)
+                    morrisons_table_data = []
+                    morrisons_table_data.append({
+                        'Product link: ': [productLink],
+                        'Product name: ': [productName],
+                        'Image': [productImage],
+                        'Price: ': [productPrice],
+                        'Price per unit: ': [productUnitPrice],
+                        'Unit': [unit]
+                        })
+                    with open('m_data.txt', 'w') as outfile:
+                        json.dump(morrisons_table_data, outfile)
+                else:
+                    pass
+    else:
+        print("Not available!")
     print("Thank you for using our service!")
 else:
     print("Please try again later!")
